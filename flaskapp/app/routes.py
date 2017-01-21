80c56ecde3953c1383d4bf825dbd960ad1c9a619
Joseph Haymaker <josepma@seas.upenn.edu>
2017-01-21 13:58:34 -0500

    front end modifications, text box added, intro page added, centering

--- flaskapp/app/routes.py
+++ flaskapp/app/routes.py
@@ -2,8 +2,12 @@
 from flask import Flask, render_template
  
 app = Flask(__name__)      
- 
+
 @app.route('/')
+def intro():
+  return render_template('intro.html')
+ 
+@app.route('/home')
 def home():
   return render_template('home.html')
 
